28ac9279d8135fddf3c7b5ff434c113a32b031c5
Valeriy Osipov <info@vosipov.com>
2017-07-16 10:50:11 +0300

    Add redirect after adding a todo

--- src/todos/views.py
+++ src/todos/views.py
@@ -1,17 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import addForm
 from .models import Todo
 
 
 def add(request):
     form = addForm(request.POST or None)
-    form_title = 'Add Todo'
-    confirm_message = None
     if form.is_valid():
         title = form.cleaned_data['title']
         text = form.cleaned_data['text']
         todo = Todo(title=title, text=text)
         todo.save()
-        confirm_message = 'New Todo added'
-        form = None
-    return render(request, 'todos/add.html', {'form_title': form_title, 'form': form, 'confirm_message': confirm_message})
+        return redirect('/todos')
+    return render(request, 'todos/add.html', {'form': form})
+

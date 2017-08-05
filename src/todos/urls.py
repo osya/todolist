5ed838d80db12c88055876e594000a653ba0fa15
Valeriy Osipov <info@vosipov.com>
2017-08-06 01:26:15 +0300

    Add paginator

--- src/todos/urls.py
+++ src/todos/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^(?P<pk>\d+)$', views.TodoDetailView.as_view(), name='detail'),
     url(r'^create/$', views.TodoCreateView.as_view(), name='create'),
-    url(r'^$', views.TodoListView.as_view(), name='list'),
+    url(r'^(?:page/(?P<page>\d+))?$', views.TodoListView.as_view(), name='list'),
     url(r'^accounts/', include('allauth.urls')),
 ]
 
--- src/todos/views.py
+++ src/todos/views.py
@@ -13,9 +13,15 @@ class TodoCreateView(views.SetHeadlineMixin, views.LoginRequiredMixin, generic.C
 class TodoDetailView(views.LoginRequiredMixin, generic.DetailView):
     model = Todo
 
+    def get_context_data(self, **kwargs):
+        context = super(TodoDetailView, self).get_context_data(**kwargs)
+        context['page'] = self.request.GET.get('page', 1)
+        return context
+
 
 class TodoListView(views.LoginRequiredMixin, generic.ListView):
-    queryset = Todo.objects.all().order_by('-created_at')[:10]
+    queryset = Todo.objects.all().order_by('-created_at')
+    paginate_by = 10
 
 # TODO: Сделать, чтобы для каждого аккаунта отображались только его задачи, а не весь список задач
 # TODO: Create REST API

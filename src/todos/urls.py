c315a85d38ba8aa0125b6b7f812b82886ec6e38e
Valeriy Osipov <info@vosipov.com>
2017-08-10 23:47:42 +0300

    Add RedirectView

--- src/todos/urls.py
+++ src/todos/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     url(r'^(?P<pk>\d+)$', views.TodoDetailView.as_view(), name='detail'),
     url(r'^create/$', views.TodoCreateView.as_view(), name='create'),
     url(r'^$', views.TodoListView.as_view(), name='list'),
-    url(r'^accounts/', include('allauth.urls')),
 ]
 
 # TODO: Implement 2FA & update corresponding Cover letter

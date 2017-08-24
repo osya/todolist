3517c8378565329d7da53c54243642a3d176d084
Valeriy Osipov <info@vosipov.com>
2017-08-24 18:08:09 +0300

    Add REST API calls

--- src/todos/urls.py
+++ src/todos/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url, include
+from rest_framework.urlpatterns import format_suffix_patterns
 
 from todos.views import TodoDetail, TodoCreate, TodoList, TodoListApi, TodoDetailApi
 
@@ -13,6 +14,8 @@ urlpatterns = [
     url(r'^(?P<pk>\d+)$', TodoDetail.as_view(), name='detail'),
     url(r'^create/$', TodoCreate.as_view(), name='create'),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
 
 # TODO: Implement 2FA & update corresponding Cover letter
 # TODO: Implement TBA using django-allauth & update corresponding Cover letter
